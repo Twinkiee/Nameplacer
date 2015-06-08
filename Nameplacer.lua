907e83f2622d18163440534364c7ae686e203a83
Antonio Cirasole <antonio.cirasole@lumatagroup.com>
2015-06-08 18:44:03 +0200

    Further devs

--- Nameplacer.lua
+++ Nameplacer.lua
@@ -27,6 +27,10 @@ local STR_NAMEPLACER_MAIN_WND = "NameplacerConfigForm"
 local STR_UNIT_NAME_INPUT = "UnitNameInput"
 local STR_UNIT_LIST_SELECTED_BG = "BK3:UI_BK3_Holo_InsetHeader"
 local STR_UNIT_LIST_UNSELECTED_BG = "BK3:UI_BK3_Holo_InsetHeaderThin"
+local STR_BTN_FROM_CHEST_TO_BOTTOM = "ButtonFromChestToBottom"
+local STR_BTN_FROM_BOTTOM_TO_CHEST = "ButtonFromBottomToChest"
+local STR_BTN_FROM_BOTTOM_TO_CUSTOM = "ButtonFromBottomToCustom"
+local STR_BTN_FROM_CUSTOM_TO_BOTTOM = "ButtonFromCustomToBottom"
 
 -----------------------------------------------------------------------------------------------
 -- Initialization
@@ -75,20 +79,22 @@ function Nameplacer:OnDocLoaded()
       return
     end
 
-    -- unit lists
+    -- unit lists containers
     self.wndUnitListChest = self.wndMain:FindChild(STR_UNIT_LIST_NAME_CHEST)
     self.wndUnitListBottom = self.wndMain:FindChild(STR_UNIT_LIST_NAME_BOTTOM)
     self.wndUnitListCustom = self.wndMain:FindChild(STR_UNIT_LIST_NAME_CUSTOM)
+
+    -- unit grids
     self.wndUnitGridChest = self.wndUnitListChest:FindChild(STR_UNIT_GRID_NAME)
     self.wndUnitGridBottom = self.wndUnitListBottom:FindChild(STR_UNIT_GRID_NAME)
     self.wndUnitGridCustom = self.wndUnitListCustom:FindChild(STR_UNIT_GRID_NAME)
     self.wndSelectedUnitPosList = self.wndUnitListChest
-    
+
     -- buttons
-    self.wndButtonFromChestToBottom = self.wndMain:FindChild("ButtonFromChestToBottom")
-    self.wndButtonFromBottomToChest = self.wndMain:FindChild("ButtonFromBottomToChest")
-    self.wndButtonFromBottomToCustom = self.wndMain:FindChild("ButtonFromBottomToCustom")
-    self.wndButtonFromCustomToBottom = self.wndMain:FindChild("ButtonFromCustomToBottom")
+    self.wndButtonFromChestToBottom = self.wndMain:FindChild(STR_BTN_FROM_CHEST_TO_BOTTOM)
+    self.wndButtonFromBottomToChest = self.wndMain:FindChild(STR_BTN_FROM_BOTTOM_TO_CHEST)
+    self.wndButtonFromBottomToCustom = self.wndMain:FindChild(STR_BTN_FROM_BOTTOM_TO_CUSTOM)
+    self.wndButtonFromCustomToBottom = self.wndMain:FindChild(STR_BTN_FROM_CUSTOM_TO_BOTTOM)
 
     self.tUnitLists = { [STR_UNIT_LIST_NAME_CHEST] = self.wndUnitListChest, [STR_UNIT_LIST_NAME_BOTTOM] = self.wndUnitListBottom, [STR_UNIT_LIST_NAME_CUSTOM] = self.wndUnitListCustom }
     self.tUnitGrids = { [STR_UNIT_GRID_NAME_CHEST] = self.wndUnitGridChest, [STR_UNIT_GRID_NAME_BOTTOM] = self.wndUnitGridBottom, [STR_UNIT_GRID_NAME_CUSTOM] = self.wndUnitGridCustom }
@@ -175,8 +181,8 @@ function Nameplacer:GetUnitRowIndex(strUnitName, wndUnitGrid)
   if (not wndUnitGrid) then
     return nil
   end
-  
-  Print ("wndUnitGrid:GetName(): " .. wndUnitGrid:GetName() .. "; strUnitName: " .. strUnitName)
+
+  Print("wndUnitGrid:GetName(): " .. wndUnitGrid:GetName() .. "; strUnitName: " .. strUnitName)
 
   local tRowIndex
 
@@ -209,27 +215,45 @@ function Nameplacer:PopulateItemList()
 end
 
 -------------------------------------------------------------------------
--- clear the item list
+-- Update the selected unit input box
 -------------------------------------------------------------------------
 function Nameplacer:UpdateUnitNameInput(strUnitName)
   self.strSelectedUnitName = strUnitName
   self.wndUnitNameInput:SetText(strUnitName)
 end
 
-function Nameplacer:ResetGridSelection(wndSelectedUnitPosListContainer)
-  for strUnitPosListName, wndUnitPosListContainer in pairs(self.tUnitLists) do
-
-    if (wndSelectedUnitPosListContainer:GetName() ~= strUnitPosListName) then
-      wndUnitPosListContainer:FindChild(STR_UNIT_GRID_NAME):SetCurrentRow(0)
-    end
-    
-  end
+-------------------------------------------------------------------------
+-- Update the selected unit and signal the update
+-------------------------------------------------------------------------
+function Nameplacer:UpdateSelectedUnit(strUnitName, nCodeEnumFloaterLocation, nYoffset)
+  self.strSelectedUnitName = strUnitName
+  Event_FireGenericEvent("Nameplacer_UnitNameplatePositionChanged", nCodeEnumFloaterLocation, nYoffset)
 end
 
 
-function Nameplacer:SelectList(wndSelectedUnitPosListContainer) 
+-------------------------------------------------------------------------
+-- Unselect any grid selection other than the selected one
+-------------------------------------------------------------------------
+function Nameplacer:ResetGridSelection(wndSelectedGrid)
+
+  for _, wndGrid in pairs(self.tGridLists) do
+
+    if (wndSelectedGrid ~= wndGrid) then
+      wndGrid:SetCurrentRow(0)
+    end
+  end
+end
+
+function Nameplacer:SelectUnitGridRow(nRowIndex, wndGrid)
+  wndGrid:SetCurrentRow(nRowIndex)
+  self:ResetGridSelection(wndGrid)
+  self:SelectList(wndGrid:GetParent())
+end
+
+
+function Nameplacer:SelectList(wndSelectedUnitPosListContainer)
   self.wndSelectedUnitPosList = wndSelectedUnitPosListContainer
-  
+
   Print("self.wndSelectedUnitPosList: " .. self.wndSelectedUnitPosList:GetName())
 
   local strSelectedUnitPosContainerName = wndSelectedUnitPosListContainer:GetName()
@@ -239,14 +263,13 @@ function Nameplacer:SelectList(wndSelectedUnitPosListContainer)
   for strUnitPosListName, wndUnitPosListContainer in pairs(self.tUnitLists) do
 
     Print("wndUnitPosListContainer: " .. strSelectedUnitPosContainerName)
-    
+
     if wndUnitPosListContainer:GetName() ~= wndSelectedUnitPosListContainer:GetName() then
-        
+
       wndUnitPosListContainerBackground = wndUnitPosListContainer:FindChild("Background")
       wndUnitPosListContainerBackground:SetSprite(STR_UNIT_LIST_UNSELECTED_BG)
     end
   end
-
 
   if (strSelectedUnitPosContainerName == STR_UNIT_LIST_NAME_CHEST) then
     self.wndButtonFromChestToBottom:Enable(true)
@@ -293,27 +316,26 @@ function Nameplacer:OnUnitListSelChange(wndControl, wndHandler, iRow, iCol)
   local strUnitName = wndHandler:GetCellText(iRow, iCol)
 
   self:UpdateUnitNameInput(strUnitName)
-  
---  for a, wndUnitGrid in ipairs(self.tUnitGrids) do
---    
---    Print("Grid: " .. wndUnitGrid:GetParent():GetName())
---    
---    if (wndUnitGrid ~= wndHandler) then
---      Print("Resetting grid selection: " .. wndUnitGrid:GetParent():GetName())
---      wndHandler:SetCurrentRow(0)
---    end
---  end
+
+  --  for a, wndUnitGrid in ipairs(self.tUnitGrids) do
+  --
+  --    Print("Grid: " .. wndUnitGrid:GetParent():GetName())
+  --
+  --    if (wndUnitGrid ~= wndHandler) then
+  --      Print("Resetting grid selection: " .. wndUnitGrid:GetParent():GetName())
+  --      wndHandler:SetCurrentRow(0)
+  --    end
+  --  end
 
   local wndSelectedUnitPosListContainer = wndHandler:GetParent()
   self:SelectList(wndSelectedUnitPosListContainer)
 
-  if (not self.tUnitGrids) then 
-  Print ("not self.tUnitGrids")
+  if (not self.tUnitGrids) then
+    Print("not self.tUnitGrids")
   end
-  Print (table.tostring(self.tUnitGrids))
-  Print (table.tostring(self.tUnitLists))
+  Print(table.tostring(self.tUnitGrids))
 
-  self:ResetGridSelection(wndSelectedUnitPosListContainer)
+  self:ResetGridSelection(wndHandler)
 end
 
 function Nameplacer:OnTargetUnitChanged(oTarget)
@@ -352,7 +374,7 @@ end
 --end
 
 
-function Nameplacer:OnButtonSignalButtonSelectUnitPosList( wndHandler, wndControl, eMouseButton )
+function Nameplacer:OnButtonSignalButtonSelectUnitPosList(wndHandler, wndControl, eMouseButton)
 
   Print("OnButtonSignalButtonSelectUnitPosList; wndControl: " .. wndControl:GetName())
 
@@ -360,48 +382,51 @@ function Nameplacer:OnButtonSignalButtonSelectUnitPosList( wndHandler, wndContro
   self:SelectList(wndSelectedUnitPosListContainer)
 end
 
-function Nameplacer:OnButtonSignalChangeUnitList( wndHandler, wndControl, eMouseButton )
-  
+function Nameplacer:OnButtonSignalChangeUnitList(wndHandler, wndControl, eMouseButton)
+
   local strButtonName = wndControl:GetName()
   Print("OnButtSigChangePos; wndControl: " .. wndControl:GetName())
-  
-  if (strButtonName == "ButtonFromChestToBottom") then
+
+  if (strButtonName == STR_BTN_FROM_CHEST_TO_BOTTOM) then
     self:DeleteUnitRow(self.strSelectedUnitName, self.wndUnitGridChest)
     self:AddUnitRow(self.strSelectedUnitName, self.wndUnitGridBottom)
     self:SelectList(self.wndUnitListBottom)
     self:ResetGridSelection(self.wndUnitListBottom)
     self.wndUnitGridBottom:SetCurrentRow(self:GetUnitRowIndex(self.strSelectedUnitName, self.wndUnitGridBottom))
-   elseif (strButtonName == "ButtonFromBottomToChest") then
+  elseif (strButtonName == STR_BTN_FROM_BOTTOM_TO_CHEST) then
     self:DeleteUnitRow(self.strSelectedUnitName, self.wndUnitGridBottom)
     self:AddUnitRow(self.strSelectedUnitName, self.wndUnitGridChest)
     self:SelectList(self.wndUnitListChest)
     self:ResetGridSelection(self.wndUnitListChest)
     self.wndUnitGridChest:SetCurrentRow(self:GetUnitRowIndex(self.strSelectedUnitName, self.wndUnitGridChest))
-   elseif (strButtonName == "ButtonFromBottomToCustom") then
+  elseif (strButtonName == STR_BTN_FROM_BOTTOM_TO_CUSTOM) then
     self:DeleteUnitRow(self.strSelectedUnitName, self.wndUnitGridBottom)
     self:AddUnitRow(self.strSelectedUnitName, self.wndUnitGridCustom)
     self:SelectList(self.wndUnitListCustom)
     self:ResetGridSelection(self.wndUnitListCustom)
-    self.wndUnitGridCustom:SetCurrentRow(self:GetUnitRowIndex(self.strSelectedUnitName, self.wndUnitGridCustom))    
-   elseif (strButtonName == "ButtonFromCustomToBottom") then
+    self.wndUnitGridCustom:SetCurrentRow(self:GetUnitRowIndex(self.strSelectedUnitName, self.wndUnitGridCustom))
+  elseif (strButtonName == STR_BTN_FROM_CUSTOM_TO_BOTTOM) then
     self:DeleteUnitRow(self.strSelectedUnitName, self.wndUnitGridCustom)
     self:AddUnitRow(self.strSelectedUnitName, self.wndUnitGridBottom)
     self:SelectList(self.wndUnitListBottom)
     self:ResetGridSelection(self.wndUnitListBottom)
-    self.wndUnitGridBottom:SetCurrentRow(self:GetUnitRowIndex(self.strSelectedUnitName, self.wndUnitGridBottom))     
+    self.wndUnitGridBottom:SetCurrentRow(self:GetUnitRowIndex(self.strSelectedUnitName, self.wndUnitGridBottom))
   end
 end
 
-function Nameplacer:OnGridSelChangeUnitPosList(wndControl, wndHandler, iRow, iCol)
-  -- SEDU - GetCellData returns nil always...bug?
-  local id = tonumber(wndControl:GetCellText(iRow, 1))
-  local retrievedEntity =  self:RetrieveEntityById(id)
-  if(self.tmSelectedEntity ~= retrievedEntity) then
-    self.tmSelectedEntity = retrievedEntity
-    self.tmSelectedEntity:ShowHintArrow()
-  else
-    self.tmSelectedEntity = nil
+function Nameplacer:OnEditBoxChangedUnitNameInput(wndHandler, wndControl, strText)
+
+  for strUnitGridName, wndUnitGrid in pairs(self.tGridList) do
+    local nUnitRowIndex = self:GetUnitRowIndex(strText, wndUnitGrid)
+
+    if (nUnitRowIndex) then
+      self.strSelectedUnitName = strText
+      self:SelectUnitGridRow(nUnitRowIndex, wndUnitGrid)
+      return
+    end
   end
+
+  self:ResetGridSelection()
 end
 
 -----------------------------------------------------------------------------------------------
@@ -411,38 +436,38 @@ local NameplacerInst = Nameplacer:new()
 NameplacerInst:Init()
 
 
-function table.val_to_str ( v )
-  if "string" == type( v ) then
-    v = string.gsub( v, "\n", "\\n" )
-    if string.match( string.gsub(v,"[^'\"]",""), '^"+$' ) then
+function table.val_to_str(v)
+  if "string" == type(v) then
+    v = string.gsub(v, "\n", "\\n")
+    if string.match(string.gsub(v, "[^'\"]", ""), '^"+$') then
       return "'" .. v .. "'"
     end
-    return '"' .. string.gsub(v,'"', '\\"' ) .. '"'
+    return '"' .. string.gsub(v, '"', '\\"') .. '"'
   else
-    return "table" == type( v ) and table.tostring( v ) or
-      tostring( v )
+    return "table" == type(v) and table.tostring(v) or
+        tostring(v)
   end
 end
 
-function table.key_to_str ( k )
-  if "string" == type( k ) and string.match( k, "^[_%a][_%a%d]*$" ) then
+function table.key_to_str(k)
+  if "string" == type(k) and string.match(k, "^[_%a][_%a%d]*$") then
     return k
   else
-    return "[" .. table.val_to_str( k ) .. "]"
+    return "[" .. table.val_to_str(k) .. "]"
   end
 end
 
-function table.tostring( tbl )
+function table.tostring(tbl)
   local result, done = {}, {}
-  for k, v in ipairs( tbl ) do
-    table.insert( result, table.val_to_str( v ) )
-    done[ k ] = true
+  for k, v in ipairs(tbl) do
+    table.insert(result, table.val_to_str(v))
+    done[k] = true
   end
-  for k, v in pairs( tbl ) do
-    if not done[ k ] then
-      table.insert( result,
-        table.key_to_str( k ) .. "=" .. table.val_to_str( v ) )
+  for k, v in pairs(tbl) do
+    if not done[k] then
+      table.insert(result,
+        table.key_to_str(k) .. "=" .. table.val_to_str(v))
     end
   end
-  return "{" .. table.concat( result, "," ) .. "}"
+  return "{" .. table.concat(result, ",") .. "}"
 end
